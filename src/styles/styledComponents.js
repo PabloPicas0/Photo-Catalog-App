58ab1e764547138d8082a798fbd409381b2972ae
PabloPicas0 <pawel.fe@gmail.com>
2023-05-15 23:00:19 +0200

    added hover effect on navbar links

--- src/styles/styledComponents.js
+++ src/styles/styledComponents.js
@@ -4,16 +4,35 @@ export const contactSection = { display: "flex", justifyContent: "center", align
 
 export const filterBorderRadius = { borderRadius: "5px", border: "1px solid white" };
 
-export const modal = { overflowY: "scroll", overflowX: "hidden", backdropFilter: { sm: "blur(3px)", xs: "none" } };
+export const modal = {
+  overflowY: "scroll",
+  overflowX: "hidden",
+  backdropFilter: { sm: "blur(3px)", xs: "none" },
+};
 
 export const modalCard = { maxWidth: 645, margin: "0 auto" };
 
-export const appBarStyle = { backgroundColor: "transparent", padding: "20px 6vw", boxShadow: "none", textTransform: "uppercase" };
+export const appBarStyle = {
+  backgroundColor: "transparent",
+  padding: "20px 6vw",
+  boxShadow: "none",
+  textTransform: "uppercase",
+};
 
 export const appBarLinks = { display: { sm: "flex", xs: "none" } };
 
 export const appBarIcon = { color: "rgba(255, 255, 255, 0.87);", display: { xs: "inline-flex", sm: "none" } };
 
-export const mediaBarStyle = {position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 1337, marginBottom: "50px", paddingLeft: "5%", boxShadow: "none"}
+export const mediaBarStyle = {
+  position: "fixed",
+  bottom: 0,
+  left: 0,
+  right: 0,
+  zIndex: 1337,
+  marginBottom: "50px",
+  paddingLeft: "5%",
+  boxShadow: "none",
+  display: { xs: "none", lg: "inline-flex" },
+};
 
-export const mediaBarLinksStyle = {color: "rgba(255, 255, 255, 0.87)"}
+export const mediaBarLinksStyle = { color: "rgba(255, 255, 255, 0.87)" };
