673b84c29d35d2200b6df0f5bd8b6fdd77e53a0c
PabloPicas0 <pawel.fe@gmail.com>
2023-05-26 20:53:04 +0200

    added horizontal slider functionality

--- src/App.jsx
+++ src/App.jsx
@@ -133,7 +133,6 @@ function App() {
         style={catalogsSection}
         className="catalogs"
         ref={(el) => (sectionsRef.current[1] = el)}>
-        <Filters handleFilter={handleFilter} />
         <Catalog catalogs={catalogs} handleOpen={handleOpen} />
         <CatalogModal openModal={openModal} handleClose={handleClose} modal={modal} />
       </section>
--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -1,34 +1,53 @@
 import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import { useState } from "react";
 
 const Catalog = (props) => {
   const { catalogs, handleOpen } = props;
 
+  const [currentIndex, setCurrentIndex] = useState(0)
+
+  const sliders = catalogs.map((element, idx) => {
+    return (
+      <Grid item key={idx}>
+        <Card sx={{ maxWidth: 325 }}>
+          <CardActionArea onClick={() => handleOpen(element)}>
+            <CardMedia component={"img"} sx={{ height: 140 }} src={element.image} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {element.catalog}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel porro dolores neque soluta illo
+                optio ea beatae recusandae veritatis, alias maiores velit id necessitatibus, culpa nihil,
+                praesentium pariatur amet sequi.
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grid>
+    );
+  });
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0
+    const newIndex = isFirstSlide ? sliders.length - 1 : currentIndex - 1
+
+    setCurrentIndex(newIndex);
+  }
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === sliders.length - 1
+    const newIndex = isLastSlide ? 0 : currentIndex + 1
+
+    setCurrentIndex(newIndex)
+  }
+
   //TODO: Refactor to scrollable in horizontal  
   return (
     <div className="catalogs-wrapper">
-      <Grid container spacing={2} justifyContent={"center"} width={"100%"}>
-        {catalogs.map((element, idx) => {
-          return (
-            <Grid item key={idx}>
-              <Card sx={{ maxWidth: 325 }}>
-                <CardActionArea onClick={() => handleOpen(element)}>
-                  <CardMedia component={"img"} sx={{ height: 140 }} src={element.image} />
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                      {element.catalog}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel porro dolores neque soluta
-                      illo optio ea beatae recusandae veritatis, alias maiores velit id necessitatibus, culpa
-                      nihil, praesentium pariatur amet sequi.
-                    </Typography>
-                  </CardContent>
-                </CardActionArea>
-              </Card>
-            </Grid>
-          );
-        })}
-      </Grid>
+      <button onClick={goToPrevious} className="slider-left-arrow">{"<"}</button>
+      <button onClick={goToNext} className="slider-right-arrow">{">"}</button>
+      {sliders[currentIndex]}
     </div>
   );
 };
