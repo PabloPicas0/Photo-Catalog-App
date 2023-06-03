39c33ca610deb2769d67f4fca4f3767ab79220f7
PabloPicas0 <pawel.fe@gmail.com>
2023-06-03 19:18:11 +0200

    added gap between each catalog on moblie devices

--- src/styles/catalogsStyles.js
+++ src/styles/catalogsStyles.js
@@ -2,7 +2,7 @@ export const carouselStyles = {
   display: "grid",
   gridAutoFlow: "column",
   gridAutoColumns: { xs: "100%", sm: "calc((100% / 2) - 9px)", lg: "calc((100% / 3) - 12px)" },
-  gap: { xs: "0px", sm: "16px" },
+  gap: { xs: "10px", sm: "16px" },
   overflowX: "scroll",
   scrollBehavior: "smooth",
   scrollSnapType: "x mandatory",
